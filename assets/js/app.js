27183715fa951a7d72dd5461c36b27af69ea2ddf
Evgeniy Troynov <troinov174@gmail.com>
2016-11-21 12:19:04 +0500

    built phaser.js basic app template

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+;(function() {
+  var game = new Phaser.Game(500, 500, Phaser.AUTO, '#game', { preload: preload, create: create, update: update });
+
+  function preload() {}
+  function create() {}
+  function update() {}
+})();
